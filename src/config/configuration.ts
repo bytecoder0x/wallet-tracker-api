765eeda0a6dff1f9524b56a3ad164ec5d8a0bddb
Bytecoder <bytecoder0x@gmail.com>
2023-06-20 18:46:30 +0300

    feat: config module and env example

---
+++ src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    name: process.env.DB_NAME || 'wallet_tracker',
+  },
+});
