9be2ee2aed28e232aaea642ddcba182953121391
Bytecoder <bytecoder0x@gmail.com>
2023-06-21 20:13:39 +0300

    feat: users service

---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepo: Repository<User>,
+  ) {}
+
+  findById(id: number): Promise<User | null> {
+    return this.usersRepo.findOneBy({ id });
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.usersRepo.findOneBy({ email });
+  }
+
+  create(data: Partial<User>): Promise<User> {
+    const user = this.usersRepo.create(data);
+    return this.usersRepo.save(user);
+  }
+}
