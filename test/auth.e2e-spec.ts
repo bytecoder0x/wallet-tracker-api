85e7a79a13b549209dd63c850c7ce643189f1948
Bytecoder <bytecoder0x@gmail.com>
2023-06-28 20:03:13 +0300

    test: auth e2e

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,78 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Auth (e2e)', () => {
+  let app: INestApplication;
+  const email = `test_${Date.now()}@example.com`;
+  const password = 'password123';
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/auth/register (POST) creates a user and returns a token', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ email, password })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.accessToken).toBeDefined();
+      });
+  });
+
+  it('/auth/register (POST) fails on duplicate email', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ email, password })
+      .expect(409);
+  });
+
+  it('/auth/login (POST) returns a token for correct credentials', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email, password })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.accessToken).toBeDefined();
+      });
+  });
+
+  it('/auth/login (POST) fails on wrong password', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email, password: 'wrongpass' })
+      .expect(401);
+  });
+
+  it('/users/me (GET) requires auth', () => {
+    return request(app.getHttpServer()).get('/users/me').expect(401);
+  });
+
+  it('/users/me (GET) returns profile with a valid token', async () => {
+    const login = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email, password });
+
+    return request(app.getHttpServer())
+      .get('/users/me')
+      .set('Authorization', `Bearer ${login.body.accessToken}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.email).toBe(email);
+      });
+  });
+});
