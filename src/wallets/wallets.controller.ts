f39849bd54634ad086c4fbc8979be477b5cd3aca
Bytecoder <bytecoder0x@gmail.com>
2023-07-02 15:40:53 +0300

    feat: list and delete wallets

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import {
   CurrentUser,
@@ -15,5 +24,19 @@ export class WalletsController {
   @Post()
   create(@CurrentUser() user: AuthUser, @Body() dto: CreateWalletDto) {
     return this.walletsService.create(user.id, dto);
+  }
+
+  @Get()
+  findAll(@CurrentUser() user: AuthUser) {
+    return this.walletsService.findAllByUser(user.id);
+  }
+
+  @Delete(':id')
+  async remove(
+    @CurrentUser() user: AuthUser,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    await this.walletsService.remove(user.id, id);
+    return { removed: true };
   }
 }
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ethers } from 'ethers';
@@ -23,5 +27,27 @@ export class WalletsService {
       label: dto.label ?? null,
     });
     return this.walletsRepo.save(wallet);
+  }
+
+  findAllByUser(userId: number): Promise<Wallet[]> {
+    return this.walletsRepo.find({
+      where: { user: { id: userId } },
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async findOwned(userId: number, walletId: number): Promise<Wallet> {
+    const wallet = await this.walletsRepo.findOne({
+      where: { id: walletId, user: { id: userId } },
+    });
+    if (!wallet) {
+      throw new NotFoundException('wallet not found');
+    }
+    return wallet;
+  }
+
+  async remove(userId: number, walletId: number): Promise<void> {
+    const wallet = await this.findOwned(userId, walletId);
+    await this.walletsRepo.remove(wallet);
   }
 }
