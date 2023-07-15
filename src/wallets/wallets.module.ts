3214e49eb2933e509e0f8202742801be893c175c
Bytecoder <bytecoder0x@gmail.com>
2023-07-15 18:56:52 +0300

    feat: activity count in wallets list

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Wallet } from './wallet.entity';
+import { Activity } from '../activity/activity.entity';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Wallet])],
+  imports: [TypeOrmModule.forFeature([Wallet, Activity])],
   controllers: [WalletsController],
   providers: [WalletsService],
   exports: [WalletsService],
